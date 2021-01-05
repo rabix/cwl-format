82d1335bc78c35c48a1783ec3358f15d4b5332eb
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2021-01-05 09:44:22 -0500

    Pip ruamel.yaml version, remove redundant requirements.

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
     },
     include_package_data=True,
     install_requires=[
-        "ruamel.yaml >= 0.15.77",
+        "ruamel.yaml == 0.16.12",
     ],
-    extras_require={':python_version<"3.7"': ['importlib-resources']}, 
 )
