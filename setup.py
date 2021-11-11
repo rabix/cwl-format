869fc95a0e6f1c5f7cbac05a835d4ad833396050
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-11-11 18:22:34 +0100

    allow newer ruamel.yaml versions

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     },
     include_package_data=True,
     install_requires=[
-        "ruamel.yaml == 0.16.12",
+        "ruamel.yaml >= 0.16.12",
     ],
     extras_require={':python_version<"3.7"': ['importlib-resources']},
 )
